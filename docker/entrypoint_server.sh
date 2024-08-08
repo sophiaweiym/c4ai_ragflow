7dd110d533aedd9e8bc21de0acb354d2235ca926
oreh <herolxh@gmail.com>
2024-08-08 15:04:58 +0800

    update docker compose with v0.8.0

--- docker/entrypoint_server.sh
+++ docker/entrypoint_server.sh
@@ -5,5 +5,4 @@
 export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/
 
 PY=python3
-WS=1
-$PY rag/svr/task_executor.py
+$PY api/ragflow_server.py
--- docker/entrypoint_worker.sh
+++ docker/entrypoint_worker.sh
@@ -5,4 +5,5 @@
 export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/
 
 PY=python3
-$PY api/ragflow_server.py
+WS=1
+$PY rag/svr/task_executor.py
